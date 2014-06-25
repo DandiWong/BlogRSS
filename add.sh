74fadaea400ef779773298e685330011ccef857c
sitonwong <siton.w@gmail.com>
2014-06-25 18:40:17 +0800

    补充sh中去除重复行的代码段

--- add.sh
+++ add.sh
@@ -4,6 +4,6 @@ IFS="
 "
 
 for FILE in `ls ./*.opml`; do
-  grep "<outline html" $FILE | awk -F \" '{print "\["$4"\]\("$2"\) : "$6"\n\n"}' | tee README.md
+  grep "<outline html" $FILE | awk -F "title" '!a[$1]++' | awk -F \" '{print "\["$4"\]\("$2"\) : "$6"\n\n"}' | tee README.md
 
 done
